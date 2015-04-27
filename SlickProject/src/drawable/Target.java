c6094664aef4d1e4e074e641eda664e7bc9b8395
Kyle Kornetzke <kyle.kornetzke-work@uwrf.edu>
2015-04-27 13:54:45 -0500

    added project

---
+++ SlickProject/src/drawable/Target.java
@@ -0,0 +1,109 @@
+package drawable;
+
+import org.newdawn.slick.Color;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+
+public class Target {
+
+	float x, y, width, height,xSpeed,ySpeed,totalSpeed;
+
+	public Target() {
+		x = y = width = height = 0;
+	}
+
+	public Target(float x, float y, float width, float height) {
+		this.x = x;
+		this.y = y;
+		this.width = width;
+		this.height = height;
+		
+		xSpeed = 50.0f;
+		ySpeed = 60.0f;
+
+	}
+
+	public void update(GameContainer gc, int delta) {
+
+		if( x < 0 || x > gc.getWidth()-width)
+			xSpeed = -xSpeed;
+		
+		if(y < 0 || y > gc.getHeight() - height)
+			ySpeed = -ySpeed;
+		
+		x += xSpeed*delta/1000;
+		y += ySpeed*delta/1000;
+		
+		totalSpeed = (float)Math.sqrt(Math.pow(xSpeed, 2)+Math.pow(ySpeed, 2));
+		System.out.println("Total Speed:"+totalSpeed);
+	}
+
+	public void draw(GameContainer gc, Graphics g) {
+
+		g.setAntiAlias(true);
+		g.setLineWidth(2.0f);
+		g.setColor(Color.pink);
+		g.drawLine(x, y + height, x + width / 2, y);
+		g.drawLine(x + width / 2, y, x+width, y + height);
+		g.drawLine(x + width, y + height, x, y + height);
+
+	}
+
+	public float getX() {
+		return x;
+	}
+
+	public void setX(float x) {
+		this.x = x;
+	}
+
+	public float getY() {
+		return y;
+	}
+
+	public void setY(float y) {
+		this.y = y;
+	}
+
+	public float getWidth() {
+		return width;
+	}
+
+	public void setWidth(float width) {
+		this.width = width;
+	}
+
+	public float getHeight() {
+		return height;
+	}
+
+	public void setHeight(float height) {
+		this.height = height;
+	}
+
+	public float getxSpeed() {
+		return xSpeed;
+	}
+
+	public void setxSpeed(float xSpeed) {
+		this.xSpeed = xSpeed;
+	}
+
+	public float getySpeed() {
+		return ySpeed;
+	}
+
+	public void setySpeed(float ySpeed) {
+		this.ySpeed = ySpeed;
+	}
+
+	public float getCenterX(){
+		return x+width/2;
+	}
+	public float getCenterY(){
+		return y+height/2;
+	}
+	public float getTotalSpeed(){
+		return totalSpeed;
+	}
+}
---
+++ SlickProject/src/hello/SimpleSlickGame.java
@@ -0,0 +1,83 @@
+package hello;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
+import org.newdawn.slick.AppGameContainer;
+import org.newdawn.slick.BasicGame;
+import org.newdawn.slick.Color;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Image;
+import org.newdawn.slick.SlickException;
+import drawable.Target;
+
+public class SimpleSlickGame extends BasicGame{
+	float x = 200,y = 200,deg = 0;
+	float turnSpeed = 40;
+	Image image;
+	Target ship;
+	
+	public SimpleSlickGame(String gamename)
+	{
+		super(gamename);
+	}
+
+	@Override
+	public void init(GameContainer gc) throws SlickException {
+		gc.setAlwaysRender(true);
+		gc.setMultiSample(0);
+		image = new Image("Resources/Fighter150W.png");
+		x = gc.getWidth()/2-image.getWidth()/2;
+		y = gc.getHeight()/2-image.getHeight()/2;
+		
+		ship = new Target(20,20,50,50);
+		
+		
+	}
+
+	@Override
+	public void update(GameContainer gc, int i) throws SlickException {
+		float mouseX = gc.getInput().getAbsoluteMouseX();
+		float mouseY = gc.getInput().getAbsoluteMouseY();	
+
+		ship.update(gc, i);
+		
+		mouseX = ship.getCenterX();
+		mouseY = ship.getCenterY();
+		
+		double angle = -Math.atan2( gc.getWidth()/2 - mouseX, gc.getHeight()/2 - mouseY);
+		
+		deg = (float) Math.toDegrees(angle);
+		
+		
+	}
+
+	@Override
+	public void render(GameContainer gc, Graphics g) throws SlickException
+	{
+		
+		g.rotate(gc.getWidth()/2, gc.getHeight()/2, deg);
+		g.drawImage(image, x, y);
+		g.drawLine(gc.getWidth()/2, gc.getHeight()/2, gc.getWidth()/2, -600);
+		g.rotate(gc.getWidth()/2, gc.getHeight()/2, -deg);
+		
+		ship.draw(gc, g);
+	}
+
+	public static void main(String[] args)
+	{
+		try
+		{
+			AppGameContainer appgc;
+			appgc = new AppGameContainer(new SimpleSlickGame("Simple Slick Game"));
+			//appgc.setMultiSample(2);
+			appgc.setDisplayMode(640, 480, false);
+			appgc.start();
+		}
+		catch (SlickException ex)
+		{
+			Logger.getLogger(SimpleSlickGame.class.getName()).log(Level.SEVERE, null, ex);
+		}
+	}
+}
